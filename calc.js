e9c84df7cde3a0491f8f85c0d23433b1301b8ff3
archways404 <archways@gmx.us>
2025-05-09 09:54:12 +0200

    init

---
+++ calc.js
@@ -0,0 +1,55 @@
+// CRAFTING COMPONENTS
+const E_Type_Canister = 59271;
+const Hammer_Drill = 29135;
+const Digital_Caliper = 15154;
+const Basic_Fuel = 13802;
+const Angle_Grinder = 3792;
+const Gunpowder = 19530;
+const Basic_Ammo_Production_Part = 19602;
+const Power_Supply = 5518;
+const Audio_Player = 8272;
+const Electric_Breaching_Hammer = 3845;
+const Cordless_Portable_Drill = 27766;
+const Boxed_Drip_Coffee = 210392;
+const Cutlass = 23227;
+
+// DEPENDENCIES
+const Intermediate_Ammo_Production_Part = (Boxed_Drip_Coffee + Cutlass) / 4;
+const Premium_Fuel = (E_Type_Canister + Hammer_Drill) / 4;
+
+// CRAFTING COMPONENTS (AMMO)
+const JHP = 0;
+const SS197SR = 0;
+const DBP87 = 0;
+const T45M = 0;
+const PST = 0;
+
+// AMMO PRICING (1x)
+
+const M62 = 4617;
+const M80 = 2044;
+const M995 = 3970;
+const M855A1 = 1536;
+const SS190 = 4027;
+const SS193 = 1619;
+const AP_762 = 3830;
+const BP_762 = 1900;
+const BT_762R = 4062;
+const LPS_762R = 2027;
+const AP_SX = 3380;
+const FMJ_SX = 1354;
+const BS_545 = 4091;
+const BT_545 = 1513;
+const BP_9 = 3986;
+const SP6 = 1715;
+const DVC12 = 4031;
+const DBP10 = 1850;
+const BLK_GOLD = 5095;
+const BLK_PURPLE = 2525;
+const PS12B = 3820;
+const PS12 = 2171;
+const PD12 = 2615;
+const Hybrid_68 = 4033;
+const FMJ_68 = 2218;
+const ACP_AP = 2131;
+const PBP_9 = 1741;
