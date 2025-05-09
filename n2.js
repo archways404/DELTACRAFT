edefa7c26bf09870d01375ff99f3d88d1e1ca6fb
archways <archways@gmx.us>
2025-05-09 16:16:20 +0200

    Update n2.js

--- n2.js
+++ n2.js
@@ -2,55 +2,55 @@ import chalk from 'chalk';
 import Table from 'cli-table3';
 
 // COMPONENT BASE PRICES
-const E_Type_Canister = 59271;
-const Hammer_Drill = 29135;
-const Digital_Caliper = 15154;
-const Basic_Fuel = 13802;
-const Angle_Grinder = 3792;
-const Gunpowder = 19530;
-const Basic_Ammo_Production_Part = 19602;
-const Power_Supply = 5518;
-const Audio_Player = 8272;
-const Electric_Breaching_Hammer = 3845;
-const Cordless_Portable_Drill = 27766;
+const E_Type_Canister = 56306;
+const Hammer_Drill = 30140;
+const Digital_Caliper = 18593;
+const Basic_Fuel = 16912;
+const Angle_Grinder = 4241;
+const Gunpowder = 23250;
+const Basic_Ammo_Production_Part = 23758;
+const Power_Supply = 6241;
+const Audio_Player = 8323;
+const Electric_Breaching_Hammer = 4198;
+const Cordless_Portable_Drill = 33835;
 const Boxed_Drip_Coffee = 210392;
 const Cutlass = 23227;
 
 // AMMO PRICING (1x)
-const M62 = 4617;
-const M80 = 2044;
-const M995 = 3970;
-const M855A1 = 1536;
-const SS190 = 4027;
-const SS193 = 1619;
-const AP_762 = 3830;
-const BP_762 = 1900;
-const BT_762R = 4062;
-const LPS_762R = 2027;
-const AP_SX = 3380;
-const FMJ_SX = 1354;
-const BS_545 = 4091;
-const BT_545 = 1513;
-const BP_9 = 3986;
-const SP6 = 1715;
-const DVC12 = 4031;
-const DBP10 = 1850;
-const BLK_GOLD = 5095;
-const BLK_PURPLE = 2525;
-const PS12B = 3820;
-const PS12 = 2171;
-const PD12 = 2615;
-const Hybrid_68 = 4033;
-const FMJ_68 = 2218;
-const ACP_AP = 2131;
-const PBP_9 = 1741;
+const M62 = 4867;
+const M80 = 1898;
+const M995 = 4927;
+const M855A1 = 2001;
+const SS190 = 4182;
+const SS193 = 1593;
+const AP_762 = 4296;
+const BP_762 = 2241;
+const BT_762R = 4414;
+const LPS_762R = 1736;
+const AP_SX = 3447;
+const FMJ_SX = 1570;
+const BS_545 = 4459;
+const BT_545 = 1766;
+const BP_9 = 4224;
+const SP6 = 1841;
+const DVC12 = 4655;
+const DBP10 = 1813;
+const BLK_GOLD = 3993;
+const BLK_PURPLE = 1372;
+const PS12B = 6943;
+const PS12 = 2537;
+const PD12 = 2367;
+const Hybrid_68 = 4219;
+const FMJ_68 = 2024;
+const ACP_AP = 2065;
+const PBP_9 = 1996;
 
 // COMPONENT AMMO PRICING (1x)
-const T45M = 0;
-const SS197SR = 0;
-const DBP87 = 0;
-const JHP = 0;
-const PST = 0;
+const T45M = 120;
+const SS197SR = 103;
+const DBP87 = 99;
+const JHP = 120;
+const PST = 84;
 
 // COMBINED COMPONENTS
 const Intermediate_Ammo_Production_Part = (Boxed_Drip_Coffee + Cutlass) / 4;
@@ -353,7 +353,7 @@ const calculateProfit = (recipe) => {
 		cost += unitPrice * qty;
 	}
 
-	const tax = recipe.quantity * recipe.marketPrice * 0.13;
+	const tax = recipe.quantity * recipe.marketPrice * 0.1;
 	const sellValue = recipe.quantity * recipe.marketPrice;
 	const marketSellValue = sellValue - tax;
 	const profit = marketSellValue - cost;
